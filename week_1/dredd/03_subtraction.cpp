dfdbd296375bfa81163fa980bb48e704a6d07cc0
Inguim <igorazevedost@gmail.com>
2026-08-21 12:44:37 -0300

    feat(week_1/dredd): adiciona resolucao para questão 3

---
+++ week_1/dredd/03_subtraction.cpp
@@ -0,0 +1,28 @@
+// Escreva um programa para calcular quantas vezes é possível subtrair 3 de N. O usuário deve entrar com o N inteiro positivo.
+// Para isso, utilize o código fornecido. Baixe-o, faça as correções necessárias e submeta para avaliação.
+// Entradas:
+//     Número inteiro positivo.
+// Saídas:
+//     Número de vezes que é possível subtrair 3 do número.
+// Exemplo de Entrada:
+// 15
+// Exemplo de Saída:
+// 5
+// Exemplo de Entrada:
+// 22
+// Exemplo de Saída:
+// 7
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int numero;
+	cin >> numero;
+	cout << numero / 3;
+  
+    return 0;
+}
